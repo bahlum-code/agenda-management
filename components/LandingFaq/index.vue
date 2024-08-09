6f8e96451e8c226a6c5239e8027ffcf705755f6d
gonzalojpv <gonzalojpv@gmail.com>
2024-08-09 15:19:45 -0600

    GP, make happy prettier

--- components/LandingFaq/index.vue
+++ components/LandingFaq/index.vue
@@ -1,13 +1,6 @@
 <script setup>
-import {
-  Disclosure,
-  DisclosureButton,
-  DisclosurePanel,
-} from "@headlessui/vue";
-import {
-  MinusSmallIcon,
-  PlusSmallIcon,
-} from "@heroicons/vue/24/outline";
+import { Disclosure, DisclosureButton, DisclosurePanel } from "@headlessui/vue";
+import { MinusSmallIcon, PlusSmallIcon } from "@heroicons/vue/24/outline";
 
 const faqs = [
   {
@@ -17,51 +10,48 @@ const faqs = [
   },
   // More questions...
 ];
-
 </script>
 
 <template>
-    <div>
-        <div class="mx-auto mt-32 max-w-7xl px-6 sm:mt-56 lg:px-8">
-        <div class="mx-auto max-w-4xl divide-y divide-gray-900/10">
-          <h2
-            class="text-2xl font-bold leading-10 tracking-tight text-gray-900"
+  <div>
+    <div class="mx-auto mt-32 max-w-7xl px-6 sm:mt-56 lg:px-8">
+      <div class="mx-auto max-w-4xl divide-y divide-gray-900/10">
+        <h2 class="text-2xl font-bold leading-10 tracking-tight text-gray-900">
+          Frequently asked questions
+        </h2>
+        <dl class="mt-10 space-y-6 divide-y divide-gray-900/10">
+          <Disclosure
+            v-for="faq in faqs"
+            :key="faq.question"
+            v-slot="{ open }"
+            as="div"
+            class="pt-6"
           >
-            Frequently asked questions
-          </h2>
-          <dl class="mt-10 space-y-6 divide-y divide-gray-900/10">
-            <Disclosure
-              v-for="faq in faqs"
-              :key="faq.question"
-              v-slot="{ open }"
-              as="div"
-              class="pt-6"
-            >
-              <dt>
-                <DisclosureButton
-                  class="flex w-full items-start justify-between text-left text-gray-900"
-                >
-                  <span class="text-base font-semibold leading-7">{{
-                    faq.question
-                  }}</span>
-                  <span class="ml-6 flex h-7 items-center">
-                    <PlusSmallIcon
-                      v-if="!open"
-                      class="h-6 w-6"
-                      aria-hidden="true"
-                    />
-                    <MinusSmallIcon v-else class="h-6 w-6" aria-hidden="true" />
-                  </span>
-                </DisclosureButton>
-              </dt>
-              <DisclosurePanel as="dd" class="mt-2 pr-12">
-                <p class="text-base leading-7 text-gray-600">
-                  {{ faq.answer }}
-                </p>
-              </DisclosurePanel>
-            </Disclosure>
-          </dl>
-        </div>
+            <dt>
+              <DisclosureButton
+                class="flex w-full items-start justify-between text-left text-gray-900"
+              >
+                <span class="text-base font-semibold leading-7">{{
+                  faq.question
+                }}</span>
+                <span class="ml-6 flex h-7 items-center">
+                  <PlusSmallIcon
+                    v-if="!open"
+                    class="h-6 w-6"
+                    aria-hidden="true"
+                  />
+                  <MinusSmallIcon v-else class="h-6 w-6" aria-hidden="true" />
+                </span>
+              </DisclosureButton>
+            </dt>
+            <DisclosurePanel as="dd" class="mt-2 pr-12">
+              <p class="text-base leading-7 text-gray-600">
+                {{ faq.answer }}
+              </p>
+            </DisclosurePanel>
+          </Disclosure>
+        </dl>
       </div>
     </div>
+  </div>
 </template>
