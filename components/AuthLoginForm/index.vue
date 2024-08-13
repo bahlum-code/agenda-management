e75d7d9824708eb76f9b9b968215b8bb0f61f8f6
gonzalojpv <gonzalojpv@gmail.com>
2024-08-13 15:45:22 -0600

    GP, Feedbacl Jonathan

--- components/AuthLoginForm/index.vue
+++ components/AuthLoginForm/index.vue
@@ -1,98 +1,97 @@
 <script setup lang="ts">
-const emit = defineEmits(['login-response'])
-const username = ref()
-const password = ref()
+type LoginProps = {
+  username: string;
+  password: string;
+};
+
+const emit = defineEmits<{
+  (e: "submit", form: LoginProps): void;
+}>();
+
+const form = reactive<LoginProps>({
+  username: "",
+  password: "",
+});
 
 async function handleSubmit() {
-  try {
-    const response = await fetch('/api/login', {
-      method: 'POST',
-      headers: { 'Content-Type': 'application/json' },
-      body: JSON.stringify({ username: username.value, password: password.value })
-    });
-    const data = await response.json();
-
-    emit('login-response', { success:'ok', data})
-
-  } catch (error) {
-    emit('login-response',{success:"error", error})
-  }
+  //TODO: Add form validation
+  //TODO: Add form submission
+  emit("submit", form);
 }
 </script>
 <template>
-    <div class="mt-10 sm:mx-auto sm:w-full sm:max-w-sm">
-      <form
-        class="space-y-6"
-        action="#"
-        method="POST"
-        @submit.prevent="handleSubmit"
-      >
-        <div>
-          <label
-            for="email"
-            class="block text-sm font-medium leading-6 text-gray-900"
-            >Email address</label
-          >
-          <div class="mt-2">
-            <input
-              id="email"
-              v-model="username"
-              name="email"
-              type="email"
-              autocomplete="email"
-              required
-              class="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
-            >
-          </div>
-        </div>
-
-        <div>
-          <div class="flex items-center justify-between">
-            <label
-              for="password"
-              class="block text-sm font-medium leading-6 text-gray-900"
-              >Password</label
-            >
-            <div class="text-sm">
-              <NuxtLink
-                to="/auth/forgot-password"
-                class="font-semibold text-indigo-600 hover:text-indigo-500"
-                >Forgot password?</NuxtLink
-              >
-            </div>
-          </div>
-          <div class="mt-2">
-            <input
-              id="password"
-              v-model="password"
-              name="password"
-              type="password"
-              autocomplete="current-password"
-              required
-              class="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
-            >
-          </div>
-        </div>
-
-        <div>
-          <button
-            type="submit"
-            class="flex w-full justify-center rounded-md bg-indigo-600 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
-          >
-            Sign in
-          </button>
-        </div>
-      </form>
-
-      <p class="mt-10 text-center text-sm text-gray-500">
-        Not a member?
-        {{ " " }}
-        <a
-          href="#"
-          class="font-semibold leading-6 text-indigo-600 hover:text-indigo-500"
-          >Start a 14 day free trial</a
+  <div class="mt-10 sm:mx-auto sm:w-full sm:max-w-sm">
+    <form
+      class="space-y-6"
+      action="#"
+      method="POST"
+      @submit.prevent="handleSubmit"
+    >
+      <div>
+        <label
+          for="email"
+          class="block text-sm font-medium leading-6 text-gray-900"
+          >Email address</label
         >
-      </p>
-    </div>
-</template>
+        <div class="mt-2">
+          <input
+            id="email"
+            v-model="form.username"
+            name="email"
+            type="email"
+            autocomplete="email"
+            class="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+          />
+          <p>this field is required</p>
+        </div>
+      </div>
 
+      <div>
+        <div class="flex items-center justify-between">
+          <label
+            for="password"
+            class="block text-sm font-medium leading-6 text-gray-900"
+            >Password</label
+          >
+          <div class="text-sm">
+            <NuxtLink
+              to="/auth/forgot-password"
+              class="font-semibold text-indigo-600 hover:text-indigo-500"
+              >Forgot password?</NuxtLink
+            >
+          </div>
+        </div>
+        <div class="mt-2">
+          <input
+            id="password"
+            v-model="form.password"
+            name="password"
+            type="password"
+            autocomplete="current-password"
+            required
+            class="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+          />
+        </div>
+      </div>
+
+      <div>
+        <button
+          type="submit"
+          class="flex w-full justify-center rounded-md bg-indigo-600 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+        >
+          Sign in
+        </button>
+      </div>
+    </form>
+
+    <p class="mt-10 text-center text-sm text-gray-500">
+      Not a member?
+      {{ " " }}
+      <a
+        href="#"
+        class="font-semibold leading-6 text-indigo-600 hover:text-indigo-500"
+        >Start a 14 day free trial</a
+      >
+    </p>
+  </div>
+</template>
